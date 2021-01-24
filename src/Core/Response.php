5008d4d597487fbdff8e6915ff3528e4f9bb01fa
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-24 17:00:51 +0200

    Add Response class

---
+++ src/Core/Response.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Simple\Core;
+
+
+class Response
+{
+    /**
+     * Create a json response
+     * 
+     * @param array data
+     * @param int status
+     * @return string the encoded json string
+     */
+    public static function json(array $data, int $status)
+    {
+        $json = json_encode($data);
+        header('Content-Type: application/json');
+        http_response_code($status);
+        echo $json;
+    }
+}
