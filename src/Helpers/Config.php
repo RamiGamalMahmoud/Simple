0834c0f9a21f89191cc0319d4ffaad6cf7f25bc2
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-04-26 00:55:41 +0200

    fix names

--- src/Helpers/Config.php
+++ src/Helpers/Config.php
@@ -6,26 +6,26 @@ class Config
 {
     private static $configDirectory;
 
-    private static array $loadedConfigFiles = [];
+    private static array $configFiles = [];
 
     public static function load($configDirectory)
     {
         self::$configDirectory = $configDirectory;
     }
 
-    public static function get($name)
+    public static function get($key)
     {
-        $parts = explode('.', $name);
-        $fileName = self::$configDirectory . DIRECTORY_SEPARATOR  . $parts[0] . '.php';
-        $key = $parts[1];
+        $keyParts = explode('.', $key);
+        $fileName = self::$configDirectory . DIRECTORY_SEPARATOR  . $keyParts[0] . '.php';
+        $key = $keyParts[1];
 
-        if (key_exists($fileName, self::$loadedConfigFiles)) {
-            return self::$loadedConfigFiles[$fileName][$key];
+        if (key_exists($fileName, self::$configFiles)) {
+            return self::$configFiles[$fileName][$key];
         }
 
         if (file_exists($fileName)) {
             $arr = require $fileName;
-            self::$loadedConfigFiles[$fileName] = $arr;
+            self::$configFiles[$fileName] = $arr;
             if (key_exists($key, $arr)) {
                 return $arr[$key];
             }
